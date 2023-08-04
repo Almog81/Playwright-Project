d83d7289e3205356fc2452773411d4a8cc275b28
Almog Noach <almog81@gmail.com>
2023-08-04 12:14:34 +0300

    add Test 01-03

---
+++ tests/Automation-Playground/AutomationPlayground.spec.js
@@ -0,0 +1,30 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async({ page }) => {
+    await page.goto('http://uitestingplayground.com/');
+});
+
+test.describe('Testing Playground', () => {
+
+    test('Test01: Dynamic ID', async({ page }) => {
+        await page.getByRole('link', { name: 'Dynamic ID' }).click();
+        await expect(page.locator('.btn.btn-primary')).toHaveText('Button with Dynamic ID')
+
+    })
+
+    test.skip('Test02: ', async({ page }) => {
+        await page.getByRole('link', { name: 'Class Attribute' }).click();
+        await page.locator('.btn-primary').click();
+        const alert = await page.waitForEvent('dialog')
+        page.locator('.btn-primary').click();
+        await expect(alert.message()).toContain('Primary button pressed')
+    })
+
+    test.only('Test03: Hidden Layers', async({ page }) => {
+        await page.getByRole('link', { name: 'Hidden Layers' }).click();
+        const btn = await page.locator('#greenButton')
+        btn.click();
+        // await expect(btn).
+    })
+})
